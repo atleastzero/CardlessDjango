3a188a175fef34a61c15fa7f62d8154ab8481a89
atleastzero <megan.obryan52@gmail.com>
2021-01-01 18:19:02 -0800

    Django project base

---
+++ cardless/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cardless project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardless.settings')
+
+application = get_asgi_application()
